a281f06cfb8f7a86067adccd3416e3cd4c2b851b
msnishanth45 <37323167+msnishanth45@users.noreply.github.com>
2019-07-20 14:35:40 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,8 +1,7 @@
 x=input()
 if x=='a'or x=='A'or x=='E'or x=='e'or x=='i'or x=='I'or x=='o'or x=='O'or x=='u'or x=='U': 
     print("Vowel")
-elif x>='0':
-    print("invalid")
-else:
+elif x>='a' and x<='z' or x>='A' and x<='z':
     print("consonant")
-    
+else:
+  print("invalid")
