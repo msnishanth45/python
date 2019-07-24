b574fc95a08d108cbe35fd238afb09b00cac1727
msnishanth45 <37323167+msnishanth45@users.noreply.github.com>
2019-07-24 15:06:55 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,15 @@
-
+a,b = map(int,input().split())
+ 
+for n in range(a,b + 1):
+   # initialize sum
+   sum = 0
+ 
+   # find the sum of the cube of each digit
+   temp = n
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if n == sum:
+       print(n)
