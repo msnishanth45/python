920b07de1e021381d3245906521c8d930c7608f0
msnishanth45 <37323167+msnishanth45@users.noreply.github.com>
2019-08-07 23:00:46 +0530

    Update Isomorphic.py

--- Isomorphic.py
+++ Isomorphic.py
@@ -1,20 +1,20 @@
-c,d = map(str,input().split())
-x = {}; y = {}
-def is_isomorphic(c, d):
-    if len(c) != len(d):
+a,b = map(str,input().split())
+c = {}; d = {}
+def is_isomorphic(a, b):
+    if len(a) != len(b):
         return False
     else:
-        for i, v in enumerate(c):
-            if v in x and x[v] != d[i]:
+        for i, v in enumerate(a):
+            if v in c and c[v] != b[i]:
                 return False
             else:
-                x[v] = d[i]
-            if d[i] in y and y[d[i]] != v:
+                c[v] = b[i]
+            if b[i] in d and d[b[i]] != v:
                 return False
             else:
-                y[d[i]] = v
+                d[b[i]] = v
         return True
-z=is_isomorphic(c,d)
+z=is_isomorphic(a,b)
 if (True==z):
   print("yes")
 else:
