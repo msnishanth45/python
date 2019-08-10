b2ee5ed89df62af675cd4e8b04eeabd4d6c9d26a
msnishanth45 <37323167+msnishanth45@users.noreply.github.com>
2019-08-10 23:02:04 +0530

    Create 66prime.py

---
+++ 66prime.py
@@ -0,0 +1,10 @@
+num1 = int(input())
+if num1 > 1: 
+   for i in range(2, num1//2):  
+       if (num1 % i) == 0: 
+           print("no") 
+           break
+   else: 
+       print("yes") 
+else: 
+   print("no") 
